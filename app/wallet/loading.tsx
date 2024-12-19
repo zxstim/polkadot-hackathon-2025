4fd2eff10738c75d7c0d4cb89c75d4fcfc98f479
ZxStim <thang.victor.luong@gmail.com>
2024-12-19 15:42:10 +0700

    fixed loading pages

--- app/wallet/loading.tsx
+++ app/wallet/loading.tsx
@@ -3,11 +3,15 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
+    <div className="flex flex-col gap-8 max-w-[768px] mx-auto min-h-screen items-center justify-center">
       <Skeleton className="w-[40px] h-[40px] rounded-md" />
-      <Skeleton className="w-[200px] h-[40px] rounded-md" />
-      <Skeleton className="w-[100px] h-[40px] rounded-md" />
-      <Skeleton className="w-full h-[600px] rounded-md" />
+      <div className="flex flex-col md:flex-row gap-2">
+        <Skeleton className="w-[100px] h-[40px] rounded-md" />
+        <Skeleton className="w-[100px] h-[40px] rounded-md" />
+        <Skeleton className="w-[100px] h-[40px] rounded-md" />
+        <Skeleton className="w-[100px] h-[40px] rounded-md" />
+      </div>
+      <Skeleton className="w-full h-[300px] rounded-md" />
     </div>
   )
 }
