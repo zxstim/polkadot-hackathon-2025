a739eef5481af63c7bde10fa8932d0a87172d9f2
ZxStim <thang.victor.luong@gmail.com>
2024-12-25 11:11:05 +0700

    changed main

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,10 @@ export default function Home() {
           <li className="mb-2">
             <Link href="/write-contract">Write contract</Link>
           </li>
+          <li className="mb-2">
+            <Link href="/mint-redeem-lst-bifrost">Mint/Redeem LST Bifrost</Link>
+          </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
