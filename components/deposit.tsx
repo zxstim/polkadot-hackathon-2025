bdc31849c9a575af08f483c75c29c50b0a4649c6
ZxStim <thang.victor.luong@gmail.com>
2025-01-07 11:50:52 +0700

    feat: added forms

--- components/deposit.tsx
+++ components/deposit.tsx
@@ -8,12 +8,28 @@ import {
   useReadContracts,
   useAccount
 } from "wagmi";
+import {
+  Ban,
+  ExternalLink,
+  ChevronDown,
+  X,
+  Hash,
+  LoaderCircle,
+  CircleCheck,
+  WalletMinimal,
+  HandCoins,
+  Vault,
+  RefreshCcw,
+} from "lucide-react";
 import { formatUnits } from "viem";
-import { lstokenAbi, zekaeVaultAbi, zusdAbi } from "@/lib/abis";
+import { lstokenAbi, zekaeVaultAbi } from "@/lib/abis";
 import { getSigpassWallet } from "@/lib/sigpass";
 import { useAtomValue } from "jotai";
 import { addressAtom } from "@/components/sigpasskit";
 import { localConfig } from "@/app/providers";
+import { formatBalance } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 export default function Deposit() {
@@ -26,13 +42,12 @@ export default function Deposit() {
   // get the address from session storage
   const address = useAtomValue(addressAtom);
 
+  // contract addresses
   const LST_CONTRACT_ADDRESS = "0xac329056F5d04C1125d8d3500cD2dF819eD9cA86";
-  const ZUSD_CONTRACT_ADDRESS = "0x43bF52395Da87278AA56024D8879d749ee6Fa0B2";
   const ZEKAE_VAULT_CONTRACT_ADDRESS = "0xF2cBA4d5C9A1A0b15bFCa4Db467422dcddB628e0";
 
-
   // useReadContracts hook to read contract
-  const { data, refetch: refetchBalance } = useReadContracts({
+  const { data, refetch, isFetching } = useReadContracts({
     contracts: [
       {
         address: LST_CONTRACT_ADDRESS,
@@ -59,15 +74,39 @@ export default function Deposit() {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex flex-col gap-2 border border-gray-200 rounded-lg p-4">
-        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">Your deposit</h1>
+      <div className="flex flex-col gap-4 border border-gray-200 rounded-lg p-4">
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-row items-center gap-2">
+            <Vault className="w-10 h-10" />
+            <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">Your deposit</h1>
+          </div>
+          <Button size="icon" onClick={() => refetch()}><RefreshCcw /></Button>
+        </div>
         <p className="text-lg text-muted-foreground">
-          You can deposit LST to the contract to mint zUSD.
+          You can deposit LST to the vault to mint zUSD.
         </p>
-        <h2 className="text-xl font-semibold tracking-tight">Wallet</h2>
-        <p>{formatUnits(currentLstBalance || BigInt(0), 18)} LST</p>
-        <h2 className="text-xl font-semibold tracking-tight">Deposited</h2>
-        <p>{formatUnits(depositAmount || BigInt(0), 18)} LST</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-xl font-semibold tracking-tight">Wallet</h2>
+            {isFetching ? (
+              <Skeleton className="h-6 w-32" />
+            ) : (
+              <p>{formatBalance(formatUnits(currentLstBalance || BigInt(0), 18))} LST</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <h2 className="text-xl font-semibold tracking-tight">Deposited</h2>
+            {isFetching ? (
+              <Skeleton className="h-6 w-32" />
+            ) : (
+              <p>{formatBalance(formatUnits(depositAmount || BigInt(0), 18))} LST</p>
+            )}
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          <Button variant="outline">Withdraw</Button>
+          <Button>Deposit</Button>
+        </div>
       </div>
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,8 @@ export function cn(...inputs: ClassValue[]) {
 export function truncateHash(hash: string, startLength: number = 6, endLength: number = 4) {
   return `${hash.slice(0, startLength)}...${hash.slice(-endLength)}`;
 }
+
+export function formatBalance(balance: string) {
+  // Convert string to number and format with thousand separators
+  return Number(balance).toLocaleString();
+}
